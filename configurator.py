2a9486aff79b5ccedc3a44544614515935938427
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2018-05-03 14:21:56 -0300

    Add module to read configuration from string

--- configurator.py
+++ configurator.py
@@ -2,25 +2,39 @@
 
 import ConfigParser
 import json
+import cStringIO
 import sys
 
 
+def string_config(string, defaults):
+    """ Read a Config String and Build a Configuration Dictionary. """
+    return load_config(cStringIO.StringIO(string), defaults)
+
+
 def load_config(file, defaults):
-    """ Open a Config File and Build a Configuration dDictionary. """
+    """ Open a Config File and Build a Configuration Dictionary. """
     config = ConfigParser.ConfigParser()
     configuration = defaults.copy() if isinstance(defaults, dict) else {}
 
-    # Can't open the file, return the default
-    if not config.read(file):
-        return configuration
-    
+    # Open the file
+    if isinstance(file, str):
+        # Can't open the file, return the default
+        if not config.read(file):
+            return configuration
+    # Or read the buffer
+    else:
+        try:
+            config.readfp(file)
+        except:
+            return configuration
+
     for section in config.sections():
         configuration[section] = {}
         for option in config.options(section):
             configuration[section][option] = config.get(section, option)
 
     return configuration
-    
+
 
 def main(argv):
     """
